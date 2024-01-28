58236d6f6de5f57f09c73d858e8ccd000a332ebd
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-28 08:59:28 +0300

    Style aside 'steps'

--- src/components/step/Step.jsx
+++ src/components/step/Step.jsx
@@ -1,5 +1,17 @@
-function Step() {
-  return <div>STEP</div>;
+import styles from "./step.module.css";
+
+function Step({ num, title, isActive }) {
+  return (
+    <div className={styles.step}>
+      <span className={`${styles.num} ${isActive ? styles.active : ""}`}>
+        {num}
+      </span>
+      <div>
+        <h5 className={styles.heading}>{`STEP ${num}`}</h5>
+        <p className={styles.title}>{title}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Step;
--- src/components/steps/Steps.jsx
+++ src/components/steps/Steps.jsx
@@ -1,12 +1,13 @@
 import Step from "../step/Step";
+import styles from "./steps.module.css";
 
 function Steps() {
   return (
-    <aside className="steps">
-      <Step />
-      <Step />
-      <Step />
-      <Step />
+    <aside className={styles.steps}>
+      <Step num="1" title="YOUR INFO" isActive={true} />
+      <Step num="2" title="SELECT PLAN" isActive={false} />
+      <Step num="3" title="ADD-ONS" isActive={false} />
+      <Step num="4" title="SUMMARY" isActive={false} />
     </aside>
   );
 }
