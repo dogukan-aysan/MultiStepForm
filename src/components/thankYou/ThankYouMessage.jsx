1b9638c6c133f36cc53a8a162489fb9a77303b44
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-02-01 09:29:44 +0300

    Bug fix: use class instead of plain html element for styling

--- src/components/thankYou/ThankYouMessage.jsx
+++ src/components/thankYou/ThankYouMessage.jsx
@@ -1,10 +1,10 @@
-import "./ThankYouMessage.module.css";
+import styles from "./ThankYouMessage.module.css";
 function ThankYouMessage() {
   return (
-    <section>
+    <section className={styles.container}>
       <img src="/src/assets/images/icon-thank-you.svg" />
-      <h1>Thank you!</h1>
-      <p>
+      <h1 className={styles.heading}>Thank you!</h1>
+      <p className={styles.text}>
         Thanks for confirming your subscription! We hope you have fun using our
         platform. If you ever need support, please feel free to email us at
         support@loremgaming.com.
